f9ecfa84d6109b77e5d30103d5371cc75099ad2f
Hirwach <hirwach@hotmail.com>
2021-03-11 18:22:33 -0500

    Added button to go to suggestion

--- app/src/main/java/com/example/co2monitor/MainActivity.java
+++ app/src/main/java/com/example/co2monitor/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.co2monitor;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -17,8 +20,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView1;
     private TextView textView2;
 
+    private Button suggestion;
+
     DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
     DatabaseReference co2data = mDatabase.child("co2_ppm");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,19 @@ public class MainActivity extends AppCompatActivity {
 
         textView1 = findViewById(R.id.label);
         textView2 = findViewById(R.id.co2Concentration);
+
+        suggestion = findViewById(R.id.suggestion_button);
+        suggestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSuggestionActivity();
+            }
+        });
+    }
+
+    public void openSuggestionActivity(){
+        Intent intent = new Intent(this, SuggestionActivity.class);
+        startActivity(intent);
     }
 
     @Override
