b0e79c837b393d824406d5d8cb6a28cf0e443b79
mattmor98@hotmail.com <cardesigner98>
2021-03-24 01:15:20 -0400

    Modified Info and Suggestion Activity strings.

--- app/src/main/java/com/example/co2monitor/SuggestionActivity.java
+++ app/src/main/java/com/example/co2monitor/SuggestionActivity.java
@@ -8,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import android.text.style.TypefaceSpan;
 
 import android.view.View;
 
