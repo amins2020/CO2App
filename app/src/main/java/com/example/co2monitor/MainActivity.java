edbf7b017d98c97b19b7638714760c27b56ccb47
charleschochos <charles.chochos@yahoo.com>
2021-03-28 13:54:37 -0400

    UI

--- app/src/main/java/com/example/co2monitor/MainActivity.java
+++ app/src/main/java/com/example/co2monitor/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.co2monitor;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.widget.Toolbar;
+
 //Packages used for Firebase
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-// Public and Private members used in MainActivity
+    // Public and Private members used in MainActivity
     private TextView textView1;
     private TextView textView2;
 
@@ -28,11 +36,38 @@ public class MainActivity extends AppCompatActivity {
     DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
     DatabaseReference co2data = mDatabase.child("co2_ppm");
 
-//Methods
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+
+    //Methods
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*---------Hooks--------*/
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        /*--------Toolbar------*/
+        setSupportActionBar(toolbar);
+
+        /*--------Navigation Drawer Menu-------*/
+
+        /*--- Hide or Show items ----*/
+        Menu menu = navigationView.getMenu();
+        menu.findItem(R.id.nav_logout).setVisible(false);
+        menu.findItem(R.id.nav_profile).setVisible(false);
+
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
 
         //
         textView1 = findViewById(R.id.label);//Indicates that the numbers below are CO2 ppms
@@ -57,14 +92,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
     //Method used to go to the suggestion activity
-    public void openSuggestionActivity(){
+    public void openSuggestionActivity() {
         Intent intent = new Intent(this, SuggestionActivity.class);
         startActivity(intent);
     }
 
     //Method used to go to the info activity
-    public void openInfoActivity(){
+    public void openInfoActivity() {
         Intent intent = new Intent(this, InfoActivity.class);
         startActivity(intent);
 
@@ -75,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-       co2data.addValueEventListener(new ValueEventListener() {
+        co2data.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 String co2data = dataSnapshot.getValue(String.class);
@@ -88,5 +134,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_tips:
+                Intent intent = new Intent(MainActivity.this, SuggestionActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_info:
+                Intent intent1 = new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(intent1);
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.dot_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.setting:
+                Intent i = new Intent(this, Preference.class);
+                startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
